d1163b8d87cac2a8e99f8b9cb95849e62e6ed7b8
fedealdosivi <fedepalo13@gmail.com>
2023-09-27 03:33:14 +0200

    added coverage

--- tests/dept.test.ts
+++ tests/dept.test.ts
@@ -1,4 +1,5 @@
 import test, { chromium, expect } from "playwright/test";
+import v8toIstanbul from 'v8-to-istanbul';
 
 test("Navigate through dept page", async() =>{
     const browser = await chromium.launch();
@@ -6,6 +7,8 @@ test("Navigate through dept page", async() =>{
     const page = await context.newPage();
 
     await page.goto("https://www.deptagency.com/");
+    await page.coverage.startJSCoverage();
+    await page.coverage.startCSSCoverage();
     
     try {
         await page.waitForSelector("'Accept All'", { state: 'visible', timeout: 5000 });
@@ -21,5 +24,15 @@ test("Navigate through dept page", async() =>{
         console.log('Unable to click on Careers link:', error);
     }
     await expect(page).toHaveURL("https://www.deptagency.com/careers/")
+
+    const JsCoverage = await page.coverage.stopJSCoverage();
+    const CssCoverage = await page.coverage.stopCSSCoverage();
+    console.log(JSON.stringify(JsCoverage));
+    for (const entry of JsCoverage) {
+        const converter = v8toIstanbul('', 0, { source: entry.source });
+        await converter.load();
+        converter.applyCoverage(entry.functions);
+        console.log(JSON.stringify(converter.toIstanbul()));
+      }
     await browser.close();
 })
