456201b7112334d3755357d546a58a808311c3f3
fedealdosivi <fedepalo13@gmail.com>
2023-09-27 14:35:21 +0200

    coverage report working

--- tests/dept.test.ts
+++ tests/dept.test.ts
@@ -1,14 +1,51 @@
 import test, { chromium, expect } from "playwright/test";
 import v8toIstanbul from 'v8-to-istanbul';
+import libCoverage from 'istanbul-lib-coverage';
+import libReport from 'istanbul-lib-report';
+import reports from 'istanbul-reports';
+import { Page } from "playwright";
+import { promises as fs } from 'fs';
 
-test("Navigate through dept page", async() =>{
+let page: Page;
+
+export async function saveV8Coverage(page: Page): Promise<void> {
+    const coverage = await page.coverage.stopJSCoverage();
+    const map = libCoverage.createCoverageMap();
+    for (const entry of coverage) {
+        if (entry.url === '') {
+            continue;
+        }
+            const scriptPath = `test${new URL(entry.url).pathname}`;
+            const converter = v8toIstanbul(scriptPath, 0, { source: entry.source }, (filepath) => {
+            const normalized = filepath.replace(/\\/g, '/');
+            const ret = normalized.includes('node_modules/');
+            return ret;
+        });
+        try {
+            await converter.load();
+            converter.applyCoverage(entry.functions);
+            const data = converter.toIstanbul();
+            map.merge(data);
+        } catch (error) {
+            console.error(`Error processing entry ${entry.url}:`, error);
+        }
+    }
+    await fs.rm('coverage', { force: true, recursive: true });
+    const context = libReport.createContext({ coverageMap: map });
+    reports.create('html').execute(context);
+  }
+
+test.beforeAll(async () => {
     const browser = await chromium.launch();
     const context = await browser.newContext();
-    const page = await context.newPage();
-
-    await page.goto("https://www.deptagency.com/");
+    page = await context.newPage();
     await page.coverage.startJSCoverage();
     await page.coverage.startCSSCoverage();
+});
+
+test("Navigate through dept page", async() =>{
+
+    await page.goto("https://www.deptagency.com/");
     
     try {
         await page.waitForSelector("'Accept All'", { state: 'visible', timeout: 5000 });
@@ -24,15 +61,9 @@ test("Navigate through dept page", async() =>{
         console.log('Unable to click on Careers link:', error);
     }
     await expect(page).toHaveURL("https://www.deptagency.com/careers/")
-
-    const JsCoverage = await page.coverage.stopJSCoverage();
-    const CssCoverage = await page.coverage.stopCSSCoverage();
-    console.log(JSON.stringify(JsCoverage));
-    for (const entry of JsCoverage) {
-        const converter = v8toIstanbul('', 0, { source: entry.source });
-        await converter.load();
-        converter.applyCoverage(entry.functions);
-        console.log(JSON.stringify(converter.toIstanbul()));
-      }
-    await browser.close();
 })
+
+test.afterAll(async () => {
+    await saveV8Coverage(page);
+    await page.close();
+});
