6e8f7888b26e0bdb73b436148c80488c5b7492d9
fedealdosivi <fedepalo13@gmail.com>
2023-10-02 23:40:24 +0100

    added reporter configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,12 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter:[ ["dot"], ["json",{
+    outputFile: "jsonReports/jsonReport.json"
+  }], ["html",{
+    open:"never"
+  }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     headless: true,
